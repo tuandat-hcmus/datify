b6543ccbff44650ccca3f94aadf9d333dd0d1a94
tuandat-hcmus <ngotuandat.lop9a@gmail.com>
2023-08-31 15:31:37 +0000

    name change

--- js/login.js
+++ js/login.js
@@ -8,10 +8,10 @@ let formInput = document.getElementsByClassName('container');
 
 visibleButton.addEventListener('click', () => {
     let imgSrc = visibleButton.childNodes[0].getAttribute('src');
-    if(imgSrc == './assets/hidden-pass.svg'){
+    if (imgSrc == './assets/hidden-pass.svg') {
         imgSrc = './assets/show-pass.svg';
         loginForm.psw.type = 'text';
-    } else{
+    } else {
         imgSrc = './assets/hidden-pass.svg';
         loginForm.psw.type = 'password';
     }
@@ -22,12 +22,14 @@ loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
     let usn = event.target.username.value;
     let psw = event.target.psw.value;
-    if(usn == '' || psw == ''){
+    if (usn == '' || psw == '') {
         let r = document.querySelector(':root');
         r.style.setProperty('--border-color-hover', 'red');
-        for(let i = 0; i < formInput.length; i++){
+        for (let i = 0; i < formInput.length; i++) {
             formInput[i].style.borderColor = 'red';
         }
+        let fillError = document.getElementById('fill-error');
+        fillError.textContent = 'Please fill username and password'
     }
     else {
         window.localStorage.setItem('username', usn);
