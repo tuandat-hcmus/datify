1526207034c235334537c10656d3969237c0d022
tuandat-hcmus <ngotuandat.lop9a@gmail.com>
2023-10-03 12:39:22 +0000

    update tracklist.js

--- js/tracklist.js
+++ js/tracklist.js
@@ -6,9 +6,25 @@ let topbarBackground = document.querySelector('.topbar-background');
 
 let backgroundImg = document.querySelector('.background-image');
 
+let cover = document.querySelector('.image-cover');
+
+let topbarContent = document.querySelector('.topbar-content');
+
 topbarBackground.style.opacity = 0;
+r.style.setProperty('--main-color', '#2B363A');
 
 let mainView = document.querySelector('#main-view');
 mainView.addEventListener('scroll', () => {
-    console.log(mainView.contentWindow.pageYOffset);
+    let verScroll = mainView.scrollTop;
+    console.log(mainView.scrollTop);
+    backgroundImg.style.backgroundSize = (100 - verScroll * 0.01) + '%';
+    cover.style.opacity = verScroll * 0.003 + 0.5;
+    if(verScroll >= 300) {
+        topbarBackground.style.opacity = 1;
+        topbarContent.style.opacity = 1;
+    }
+    else {
+        topbarBackground.style.opacity = 0;
+        topbarContent.style.opacity = 0;
+    }
 });
